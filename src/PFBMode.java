08e8099dfdfd199164caa3dddba49e50fac78324
eldaribragimov <eldaribragimov@icloud.com>
2018-04-19 20:49:43 +0300

    added Positive and Negetative Tests in class Testing, added PFBMode

---
+++ src/PFBMode.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class PFBMode {
+
+    public byte[] VectorGenerator(){
+        String s = "12345679"; // блок вектора инициализации 64 бит
+        return s.getBytes();
+    }
+
+    public byte[] cipher(byte[] arrayListfromfile, byte[] arraykey,byte[] vector) {
+
+        int[] arrayListkey = new KeyProvider().keyprovideencode(arraykey); // получаем массив из ключа
+
+        ArrayList<Long> arrayList = new ArrayList<>();
+        long b = 0;
+
+        // собираем блоки 64 бит из байтов и кладем их в массив
+
+        for (int i = 0 ; i < arrayListfromfile.length ; i++){
+            Long s = Byte.toUnsignedLong(arrayListfromfile[i]);
+            b = b | s;
+            if ( ( i + 1 ) % 8 == 0){
+                arrayList.add(b);
+                b = 0;
+            }
+            else
+                b = b << 8;
+        }
+
+        // собираем вектор инициализации по подобию как мы собирали блоки
+
+        b = 0;
+
+        long Vector_Long_value = 0;
+
+        for (int i = 0 ; i < vector.length ; i++){
+            Long s = Byte.toUnsignedLong(vector[i]);
+            b = b | s;
+            if ( ( i + 1 ) % 8 == 0){
+                Vector_Long_value = b;
+            }
+            else
+                b = b << 8;
+        }
+
+        arrayList.set(0,new GostChangeMethod().cryptfunction(Vector_Long_value,arrayListkey) ^ arrayList.get(0));  // определяем C(1)
+
+        // выполняем для остальных C(i>1)
+
+        for (int i = 1 ; i < arrayList.size() ; i++){
+             arrayList.set(i,new GostChangeMethod().cryptfunction(arrayList.get(i-1),arrayListkey) ^ arrayList.get(i));  // выполняем операции над блоками 64 бит
+        }
+        return new DataProvider().handlier(arrayList);
+    }
+}
+
--- src/Testing.java
+++ src/Testing.java
@@ -5,11 +5,13 @@ public class Testing {
 
     @Test
 
-    public void test()  {
+    // тестирование метода простой замены валидным ключом
+    // ожидаемое значение переменной b = true
+
+    public void SimpleChangeModeTestWithValidKey()  {
         byte[] arraydata = new DataProvider().read();  // считываем блок
         byte[] array = new GostChangeMethod().encodeprocess(arraydata,new Key().keygenerate()); //  зашифровываем блок
         array = new GostChangeMethod().decodeprocess(array,new Key().keygenerate()); // расшифровываем зашифрованный блок тем же ключом
-
 
         // проверяем что первоначальный блок совпадает с расшифрованным
 
@@ -19,10 +21,77 @@ public class Testing {
                 c = false;
         }
 
-        System.out.println(c);
-
-        Assert.assertTrue(c);
+        Assert.assertTrue(c); // тест пройден если b = true
 
     }
+
+    @Test
+
+    // тестирование метода простой замены невалидным ключом
+    // ожидаемое значение переменной b = false
+
+    public void SimpleChangeModeTestWithNOTValidKey()  {
+        byte[] arraydata = new DataProvider().read();  // считываем блок
+        byte[] key = new Key().keygenerate();
+        key[0] +=2; // чуть чуть меняем ключ
+        byte[] array = new GostChangeMethod().encodeprocess(arraydata,key); //  зашифровываем блок
+        array = new GostChangeMethod().decodeprocess(array,new Key().keygenerate()); // расшифровываем зашифрованный блок тем же ключом
+
+        // проверяем что первоначальный блок совпадает с расшифрованным
+
+        boolean c = true;
+        for (int i = 0 ; i < array.length ; i++){
+            if (array[i]!=arraydata[i])
+                c = false;
+        }
+
+        Assert.assertFalse(c);  // тест пройден если b = false
+
+    }
+
+    @Test
+
+    // тестирование режима PFB валидным ключом
+    // ожидаемое значение переменной b = true
+
+    public void PFBModeTestWithValidKey(){
+        byte[] arraydata = new DataProvider().read();  // считываем блок
+        byte[] array = new PFBMode().cipher(arraydata,new Key().keygenerate(),new PFBMode().VectorGenerator()); //  зашифровываем блок
+        array = new PFBMode().cipher(array,new Key().keygenerate(),new PFBMode().VectorGenerator());     // расшифровываем зашифрованный блок тем же ключом
+
+        // проверяем что первоначальный блок совпадает с расшифрованным
+
+        boolean c = true;
+        for (int i = 0 ; i < array.length ; i++){
+            if (array[i]!=arraydata[i])
+                c = false;
+        }
+
+        Assert.assertTrue(c);  // тест пройден если b = true
+    }
+
+    @Test
+
+    // тестирование режима PFB невалидным ключом
+    // ожидаемое значение переменной b = false
+
+    public void PFBModeTestWithNOTValidKey(){
+        byte[] arraydata = new DataProvider().read();  // считываем блок
+        byte[] key = new Key().keygenerate();
+        key[0]+=2;  // чуть чуть меняем ключ
+        byte[] array = new PFBMode().cipher(arraydata,key,new PFBMode().VectorGenerator()); //  зашифровываем блок
+        array = new PFBMode().cipher(array,new Key().keygenerate(),new PFBMode().VectorGenerator());     // расшифровываем зашифрованный блок тем же ключом
+
+        // проверяем что первоначальный блок совпадает с расшифрованным
+
+        boolean c = true;
+        for (int i = 0 ; i < array.length ; i++){
+            if (array[i]!=arraydata[i])
+                c = false;
+        }
+
+        Assert.assertFalse(c);  // тест пройден если b = false
+    }
+
 }
 
